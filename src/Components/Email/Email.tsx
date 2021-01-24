da052320bd193a9b39c0be3cbdc9dc305ca09040
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-24 20:00:48 +0500

    Personal form finished

--- src/Components/Email/Email.tsx
+++ src/Components/Email/Email.tsx
@@ -1,28 +1,24 @@
 import React, { FC } from "react";
 import * as Yup from "yup";
 import { Formik, Form } from "formik";
-import FormField from "../../Utils/FormField";
+import FormField from '../../utils/FormField';
 import Button from '@material-ui/core/Button'
-import './index.css'
+import '../index.css'
 
 interface EmailForm {
-  firstName?: string;
-  lastName?: string;
+  userName?: string;
   email?: string;
   password?: string;
 }
 
-const emailValues = {
-  firstName: "",
-  lastName: "",
+const emailValues:EmailForm = {
+  userName: "",
   email: "",
   password: "",
 };
 
 const emailValidationSchema = Yup.object({
-  firstName: Yup.string()
-    .required("Required"),
-  lastName: Yup.string()
+  userName: Yup.string()
     .required("Required"),
   email: Yup.string().email().required("Provide a valid Email address"),
   password: Yup.string()
@@ -46,14 +42,14 @@ const Email: FC<EmailForm> = () => {
 >
     {
         ({dirty,isValid}) => {
+            let index=0
             return (
                 <Form>
-                    <FormField name="firstName" label="First Name" type="text" />
-                    <FormField name="lastName" label="Last Name" type="text" />
+                    <FormField name="userName" label="User Name" type="text" />
                     <FormField name="email" label="Email Address" type="text" />
                     <FormField name="password" label="Password" type="password" />
                     <div className="buttonGroup">
-                        <Button variant="contained" color="secondary"  >Back</Button>
+                        <Button disabled={index===0 ? true : false} variant="contained" color="secondary"  >Back</Button>
                         <Button variant="contained" color="primary"  >Next</Button>
                     </div>
 
--- src/Components/Personal/Personal.tsx
+++ src/Components/Personal/Personal.tsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React,{FC} from 'react'
+import * as Yup from 'yup'
+import {Formik , Form } from 'formik'
+import FormField from '../../utils/FormField'
+import Button from '@material-ui/core/Button'
+import '../index.css'
 
-const Personal = () => {
+interface PersonalFormValues{
+    firstName?: string,
+    lastName?: string,
+    address?: string,
+    city?:string,
+    province?:string,
+    country?: string,
+    phone?: string,
+}
+
+const personaInitialValues:PersonalFormValues ={
+    firstName: '',
+    lastName: '',
+    address:'',
+    city: '',
+    province:'',
+    country:'',
+    phone:''
+}
+
+const personalValidationSchema = Yup.object({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    address:Yup.string().required('Required'),
+    city: Yup.string().required('Required'),
+    province: Yup.string().required('Required'),
+    country: Yup.string().required('Required'),
+    phone: Yup.string().required('Required')
+})
+
+const personalOnSubmit=(values : PersonalFormValues) : void=>{
+    alert('')
+}
+
+const Personal:FC<PersonalFormValues> = () => {
+    let index=1
     return (
         <div>
-            Perosnal
+            <Formik
+                initialValues={personaInitialValues}
+                validationSchema={personalValidationSchema}
+                onSubmit={personalOnSubmit}
+            >
+                {
+                    ({dirty,isValid}) => (
+                        <Form>
+                            <FormField 
+                                name='firstName'
+                                label='First Name'
+                                type='text'
+                            />
+                            <FormField 
+                                name='lastName'
+                                label='Last Name'
+                                type='text'
+                            />
+                            <FormField 
+                                name='address'
+                                label='Address'
+                                type='text'
+                            />
+                            <FormField 
+                                name='city'
+                                label='City'
+                                type='text'
+                            />
+                            <FormField 
+                                name='province'
+                                label='Province or State'
+                                type='text'
+                            />
+                            <FormField 
+                                name='country'
+                                label='Country'
+                                type='text'
+                            />
+                            <FormField 
+                                name='phone'
+                                label='Phone'
+                                type='text'
+                            />
+                            <div className="buttonGroup">
+                        <Button disabled={index===0 ? true : false} variant="contained" color="secondary"  >Back</Button>
+                        <Button variant="contained" color="primary"  >Next</Button>
+                    </div>
+                        </Form>
+                    )
+                }
+            </Formik>
         </div>
     )
 }
